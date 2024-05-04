f7b4918b32cdfdaae4ddfe95017699f06a641997
savaariuttam <uttam@onlinesavaari.com>
2024-05-04 08:44:57 +0530

    4may push

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -3,12 +3,37 @@ import React from 'react';
 const SearchFlightsButton = () => {
     return (
         <div className="text-right mt-4">
-            <button className="btn btn-red">
+            {/* <button className="btn btn-red">
                 Search Flights
                 <span><i className="la la-long-arrow-right la-lg"></i></span>
-            </button>
+            </button> */}
+            <a href="/details" rel="noopener noreferrer" className="btn btn-red">
+                    Search Flights
+                    <span><i className="la la-long-arrow-right la-lg"></i></span>
+                 </a>
         </div>
     );
 };
 
 export default SearchFlightsButton;
+
+
+
+// import React from 'react';
+// import { Link } from 'react-router-dom'
+// const SearchFlightsButton = () => {
+
+
+//     return (
+//         <>
+//             <div className="text-right mt-4">
+//                 <Link to="/DetailsPage" target="_blank" rel="noopener noreferrer" className="btn btn-red">
+//                     Search Flights
+//                     <span><i className="la la-long-arrow-right la-lg"></i></span>
+//                 </Link>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default SearchFlightsButton;
